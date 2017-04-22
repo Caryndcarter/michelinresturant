2f9c37edc5c8b135139ba7482a109ddbb978e88e
Benjamin Williams <ben@benjaminwilliams.com>
2017-04-22 10:33:29 -0500

    app inital

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+app.get("/", function(req, res) {
+  // send index.html
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", function(req, res) {
+  // send tables.html
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  // send reserve.html
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
